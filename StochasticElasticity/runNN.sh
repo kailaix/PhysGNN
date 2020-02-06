73bc5f67acd9a5e9896a1f9ab7fbf85b18730270
Kailai Xu <kailaix@stanford.edu>
2020-02-05 22:14:01 -0800

    fix run

--- StochasticElasticity/runNN.sh
+++ StochasticElasticity/runNN.sh
@@ -3,8 +3,10 @@ do
 for hmat_idx in 1 2 3
 do 
 for latent_dim in 10 20 40 80 160
+do 
 julia nn.jl $hmat_idx $tid $latent_dim &
 # julia gs.jl $tid $hmat_idx &
 done 
 wait %1 %2 %3 %4 %5 %6 %7 %8 %9 %10 %11 %12 %13 %14 %15
+done 
 done 
